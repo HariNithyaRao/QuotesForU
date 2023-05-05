55bb7ae15bfd83a6251eede5dcecfdaf03c8b4d5
Harinithya <73685642+HariNithyaRao@users.noreply.github.com>
2023-05-05 19:55:36 +0530

    Update 1_Welcome.py

--- 1_Welcome.py
+++ 1_Welcome.py
@@ -1,9 +1,9 @@
 import streamlit as st
 st.set_page_config(page_title="Welcome", page_icon=":wave:", layout="wide")
 
-#st.markdown("<link href='https://fonts.googleapis.com/css2?family=Roboto&display=swap' rel='stylesheet'>", unsafe_allow_html=True)
+st.markdown("<link href='https://fonts.googleapis.com/css2?family=Roboto&display=swap' rel='stylesheet'>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; color: purple; font-family:Rampart One;font-size: 60px;'>Welcome</h1>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center; color: purple; font-family:Bebas Neue;font-size: 30px;'>Welcome to Quotes For U! This app generates a new quote for you each day.</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: purple; font-family:Bebas Neue;font-size: 30px;'>Welcome to Quotes For U! This app generates a new quote for you each day.</h1>", unsafe_allow_html=True)
 
 #st.markdown("<h1 style='text-align: center; color: purple; font-family:Edwardian Script ITC;'>Welcome</h1>", unsafe_allow_html=True)
 

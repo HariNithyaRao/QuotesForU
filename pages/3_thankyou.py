033dea22a002708c44c81034ca6891dee6b2cc8c
Harinithya <73685642+HariNithyaRao@users.noreply.github.com>
2023-05-05 20:23:37 +0530

    Update 3_thankyou.py

--- pages/3_thankyou.py
+++ pages/3_thankyou.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.set_page_config(page_title="THANK YOU!!", page_icon=":sparkles:", layout="wide")
 st.markdown("<link href='https://fonts.googleapis.com/css2?family=Rampart One&display=swap' rel='stylesheet'>", unsafe_allow_html=True)
 
-st.markdown("<h1 style='text-align: center; color: purple; font-family:Rampart One;'>THANK YOU!!</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: purple; font-size: 70px;font-family:Rampart One;'>THANK YOU!!</h1>", unsafe_allow_html=True)

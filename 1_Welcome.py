ec101aca3b4d08ccc62e01e67e02495cf3dc698f
Harinithya <73685642+HariNithyaRao@users.noreply.github.com>
2023-05-05 18:56:40 +0530

    Update 1_Welcome.py

--- 1_Welcome.py
+++ 1_Welcome.py
@@ -4,3 +4,6 @@ st.set_page_config(page_title="Welcome", page_icon=":wave:", layout="wide")
 st.markdown("<link href='https://fonts.googleapis.com/css2?family=Roboto&display=swap' rel='stylesheet'>", unsafe_allow_html=True)
 
 st.markdown("<h1 style='text-align: center; color: purple; font-family:Edwardian Script ITC;'>Welcome</h1>", unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    main()

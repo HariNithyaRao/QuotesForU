03ea701b3a4fb15f8d6e12cb969b222f82f5ad81
Harinithya <73685642+HariNithyaRao@users.noreply.github.com>
2023-05-05 18:50:35 +0530

    Add files via upload

---
+++ 1_Welcome.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.set_page_config(page_title="Welcome", page_icon=":wave:", layout="wide")
+
+st.markdown("<link href='https://fonts.googleapis.com/css2?family=Roboto&display=swap' rel='stylesheet'>", unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: center; color: purple; font-family:Edwardian Script ITC;'>Welcome</h1>", unsafe_allow_html=True)

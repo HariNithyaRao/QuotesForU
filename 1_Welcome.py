0444fec5729130144a5ed8be4315c7311756e5fa
Harinithya <73685642+HariNithyaRao@users.noreply.github.com>
2023-05-13 15:36:51 +0530

    Update 1_Welcome.py

--- 1_Welcome.py
+++ 1_Welcome.py
@@ -3,7 +3,7 @@ st.set_page_config(page_title="Welcome", page_icon=":wave:", layout="wide")
 
 st.markdown("<link href='https://fonts.googleapis.com/css2?family=Rampart One&display=swap' rel='stylesheet'>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; color: purple; font-family:Rampart One;font-size: 60px;'>Welcome</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center; color: violet; font-family:Roboto Slab;font-size: 30px;'>Welcome to Quotes For U! This app generates a new quote for you each day.</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: black; font-family:Roboto Slab;font-size: 30px;'>Welcome to Quotes For U! This app generates a new quote for you each day.</h1>", unsafe_allow_html=True)
 
 
 

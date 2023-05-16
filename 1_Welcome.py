796b89dcde07eee4cc71b6e3c53d8b0f353fe612
Harinithya <73685642+HariNithyaRao@users.noreply.github.com>
2023-05-16 17:29:00 +0530

    Update 1_Welcome.py

--- 1_Welcome.py
+++ 1_Welcome.py
@@ -1,9 +1,27 @@
 import streamlit as st
+import  base64
 st.set_page_config(page_title="Welcome", page_icon=":wave:", layout="wide")
 
-st.markdown("<link href='https://fonts.googleapis.com/css2?family=Rampart One&display=swap' rel='stylesheet'>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center; color: purple; font-family:Rampart One;font-size: 60px;'>Welcome</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center; color: black; font-family:Roboto Slab;font-size: 30px;'>Welcome to Quotes For U! This app generates a new quote for you each day.</h1>", unsafe_allow_html=True)
+def set_bg_hack(main_bg):
+    '''
+    A function to unpack an image from root folder and set as bg.
 
+    Returns
+    -------
+    The background.
+    '''
+    # set bg name
+    main_bg_ext = "png"
 
-
+    st.markdown(
+        f"""
+         <style>
+         .stApp {{
+             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+             background-size: cover
+         }}
+         </style>
+         """,
+        unsafe_allow_html=True
+    )
+set_bg_hack("Welcome.png")

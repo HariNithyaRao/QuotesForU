4c75975b3aa48780bbd9c195812a7bac99819a08
Harinithya <73685642+HariNithyaRao@users.noreply.github.com>
2023-05-16 17:56:04 +0530

    Update 2_🖋️_QuotesCloud.py

--- pages/2_🖋️_QuotesCloud.py
+++ pages/2_🖋️_QuotesCloud.py
@@ -13,7 +13,7 @@ with open('123755-designers.json', 'r') as f:
     lottie_json = json.load(f)
 col1, col2 = st.columns([1, 3])
 with col1:
-    st_lottie(lottie_json, speed=1, width=120, height=120, key='lottie')
+    st_lottie(lottie_json, speed=1, width=124, height=120, key='lottie')
 with col2:
     st.markdown("<link href='https://fonts.googleapis.com/css2?family=Rammetto One&display=swap' rel='stylesheet'>",unsafe_allow_html=True)
     st.markdown("<h2><span style='text-align: center; color: violet;font-size: 70px; font-family:Rammetto One;'>Quotes</span> <span style='color: orange;font-size: 70px;font-family:Rammetto One, cursive;'>For</span> <span style='color: blue;font-size: 70px;font-family:Rammetto One, cursive;'>U</span></h2>", unsafe_allow_html=True)
@@ -22,8 +22,9 @@ colored_header(
     description="",
     color_name="violet-70",
 )
+today=datetime.today().date()
 d=st.date_input("Select a date",min_value=datetime.date(2023,3,30),
-               max_value=datetime.date(2024,6,6))
+               max_value=today)
 formatted_date = d.strftime('%Y-%m-%d')
 
 # Set up Snowflake connection details
